b23440e00c87a973d593cbf41480f7e8439eea69
ZFan7 <zfan7@slb.com>
2020-11-26 12:57:24 +0800

    refactor: duplication: extract method to service

--- MiniBlog/Controllers/ArticleController.cs
+++ MiniBlog/Controllers/ArticleController.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
+﻿using Microsoft.AspNetCore.Mvc;
 using MiniBlog.Model;
 using MiniBlog.Services;
-using MiniBlog.Stores;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace MiniBlog.Controllers
 {
@@ -14,31 +11,23 @@ namespace MiniBlog.Controllers
     [Route("[controller]")]
     public class ArticleController : ControllerBase
     {
-        private readonly IArticleStore articleStore;
-        private readonly UserService userService;
         private readonly ArticleService articleService;
 
-        public ArticleController(IArticleStore articleStore, UserService userService, ArticleService articleService)
+        public ArticleController(ArticleService articleService)
         {
-            this.articleStore = articleStore;
-            this.userService = userService;
             this.articleService = articleService;
         }
 
         [HttpGet]
         public List<Article> List()
         {
-            return articleStore.Articles.ToList();
+            return articleService.GetAllArticles();
         }
 
         [HttpPost]
         public async Task<ActionResult<Article>> Create(Article article)
         {
-            if (article.UserName != null)
-            {
-                userService.Register(new User(article.UserName));
-                articleStore.Articles.Add(article);
-            }
+            articleService.CreateArticle(article);
 
             return CreatedAtAction(nameof(GetById), new { id = article.Id }, article);
         }
--- MiniBlog/Services/ArticleService.cs
+++ MiniBlog/Services/ArticleService.cs
@@ -1,23 +1,40 @@
-﻿using System;
+﻿using MiniBlog.Model;
+using MiniBlog.Stores;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
-using MiniBlog.Model;
-using MiniBlog.Stores;
 
 namespace MiniBlog.Services
 {
     public class ArticleService
     {
         private readonly IArticleStore articleStore;
-        public ArticleService(IArticleStore articleStore)
+        private readonly UserService userService;
+        public ArticleService(IArticleStore articleStore, UserService userService)
         {
             this.articleStore = articleStore;
+            this.userService = userService;
+        }
+
+        public List<Article> GetAllArticles()
+        {
+            return articleStore.Articles;
         }
 
         public Article FoundArticle(Guid id)
         {
             return articleStore.Articles.FirstOrDefault(article => article.Id == id);
+        }
+
+        public Article CreateArticle(Article article)
+        {
+            if (article.UserName != null)
+            {
+                userService.Register(new User(article.UserName));
+                articleStore.Articles.Add(article);
+            }
+
+            return article;
         }
     }
 }
